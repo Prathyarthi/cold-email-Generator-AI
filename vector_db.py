c7e2ed4027123fe366cf82f630d2e92270cf0d97
Prathyarthi <prathyarti@gmail.com>
2024-06-27 20:37:59 +0530

    Fix bug and response to the query

--- vector_db.py
+++ vector_db.py
@@ -6,8 +6,8 @@ collection = client.create_collection(name="my_collection")
 
 collection.add(
     documents=[
-        "Hello, how are you?",
-        "I am fine, thank you.",
+        "This document is about New York",
+        "This document is about Delhi",
     ],
     ids=['id1', 'id2'],
 )
@@ -19,3 +19,13 @@ print(all_docs)
 documents = collection.get(ids=["id1"])
 
 print(documents)
+
+results = collection.query(
+    query_texts=["Query is about California"],
+    n_results=2
+)
+
+print(results)
+
+collection.delete(ids=all_docs['ids'])
+print(collection.get())
