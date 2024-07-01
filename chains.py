e7652785641a80fb6a3398f583deb8f16ae9d184
Prathyarthi <prathyarti@gmail.com>
2024-07-02 00:41:12 +0530

    Fixed code and project complete

--- chains.py
+++ chains.py
@@ -39,22 +39,18 @@ class Chain:
             {job_description}
 
             ### INSTRUCTION:
-            You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
-            the seamless integration of business processes through automated tools. 
-            Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
-            process optimization, cost reduction, and heightened overall efficiency. 
-            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
-            in fulfilling their needs.
-            Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-            Remember you are Mohan, BDE at AtliQ. 
+            You are Prathyarthi, a software engineer having all these skills(NextJS, ReactJS, NodeJS, AWS, Git & Github, Linux,
+            Express, Docker, MongoDB, Postgresql, MySQL, Redis, Tailwind CSS, Typescript, Langchain, Pinecone). You will be applying for a relevant role.
+            Also add the most relevant ones from the following links to showcase Prathyarthi's portfolio: {link_list}
+            Remember you are Prathyarthi, a software engineer. 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
-
+            
             """
         )
         chain_email = prompt_email | self.llm
         res = chain_email.invoke({"job_description": str(job), "link_list": links})
         return res.content
 
-if __name__ == "__main__":
-    print(os.getenv("GROQ_API_KEY"))
+# if __name__ == "__main__":
+#     # print(os.getenv("GROQ_API_KEY"))
